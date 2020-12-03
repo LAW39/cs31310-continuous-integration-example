6b3db42ca3a4c35cf46923a8dcfd9b5ad4ace7b0
Luke Wybar <lukewybar@gmail.com>
2020-12-03 17:50:52 +0000

    Fixed code issue added in previous build

--- src/main/java/uk/ac/aber/cs31310/FibGenerator.java
+++ src/main/java/uk/ac/aber/cs31310/FibGenerator.java
@@ -7,20 +7,20 @@ public class FibGenerator {
 
     public ArrayList<Integer> fib(int number) {
         ArrayList<Integer> values = new ArrayList<>();
-//
-//        if(number == 0 || number == 1) {
-//            values.add(number);
-//        }
-//        else if(number > 1){
-//            int t1 = 0;
-//            int t2 = 1;
-//            for(int i = 0; i < number; i++) {
-//                values.add(t1);
-//                int sum = t1 + t2;
-//                t1 = t2;
-//                t2 = sum;
-//            }
-//        }
+
+        if(number == 0 || number == 1) {
+            values.add(number);
+        }
+        else if(number > 1){
+            int t1 = 0;
+            int t2 = 1;
+            for(int i = 0; i < number; i++) {
+                values.add(t1);
+                int sum = t1 + t2;
+                t1 = t2;
+                t2 = sum;
+            }
+        }
 
         return values;
     }
